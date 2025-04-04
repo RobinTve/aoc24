d4a266b2b6c019280d0f028a27aa7c2300309365
Robin Tveråen <robintve@stud.ntnu.no>
2025-04-04 10:40:24 +0200

    add sampleinput

--- app/src/main/kotlin/fourteen/app.kt
+++ app/src/main/kotlin/fourteen/app.kt
@@ -4,10 +4,7 @@ import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 fun readInput(filename: String): List<String> = Path("14.txt").readLines()
-val lines = readInput("\"3.txt\"");
-val grid: MutableList<MutableList<Char>> = lines.map { it.toMutableList() }.toMutableList()
-var starts: MutableList<Pair<Int, Int>> = mutableListOf()
-
+val lines = readInput("");
 
 data class Coordinate(val x: Int, val y: Int)
 data class Robot(val pos: Coordinate, val velocity: Coordinate)
@@ -15,17 +12,12 @@ data class Robot(val pos: Coordinate, val velocity: Coordinate)
 val robots: MutableList<Robot> = mutableListOf()
 fun main(){
     for (line in lines){
-
-        var g = line.filterNot { it == 'p' }
-        var s = g.filterNot { it == '=' }
-        val output = s.replace(Regex(".(?=v)"), ",")
-        var f = output.filterNot { it == 'v' }
-        var values = f.split(',')
+        val values = line
+            .filterNot { it == 'p' || it == '=' }
+            .replace(Regex(".(?=v)"), ",")
+            .filterNot { it == 'v' }
+            .split(',')
         robots.add(Robot(Coordinate(values[0].toInt(), values[1].toInt()), Coordinate(values[2].toInt(), values[3].toInt())))
-    }
-    for (robot in robots){
-        println(robot.pos.x + robot.pos.y)
-        println(robot.velocity.x + robot.velocity.y)
     }
 
 }
